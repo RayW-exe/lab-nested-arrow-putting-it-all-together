5162e3e8b2c3075991aebba08e31c622bc4d0331
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-04-06 23:57:55 +0300

    LAb done

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 function createLoginTracker (userInfo){
   let attemptCount = 0
   return (passwordAttempt) =>{
-    attemptCount = attemptCount + 1
+    attemptCount++;
     if (passwordAttempt === userInfo.password ){
        if (attemptCount < 3){
         console.log("Login successful")
        }
        else{
-        console.log("Account locked due to too many failed login attempts")
+        console.log("Attempt ${attemptCount}: Login failed")
        }
     }
-    else{
-      if (attemptCount = 1){
-        console.log("Attempt 1: Login failed")
-      }
-      else if (attemptCount = 2){
-        console.log("Attempt 2: Login failed")
-      }
-      else if (attemptCount = 3){
-        console.log("Attempt 3: Login failed")
-      }
-      else if (attemptCount > 3){
-        console.log("Account locked due to too many failed login attempts")
-      }
+    if (attemptCount > 3){
+     console.log("Account locked due to too many failed login attempts") 
     }
   } 
 }
